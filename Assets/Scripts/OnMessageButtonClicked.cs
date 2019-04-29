abb7c48edf5cf62bcd21dacedee9107379ed0c90
MA Haozhe <mhz2180572509@163.com>
2019-04-29 10:28:24 +0800

    push v0.6

---
+++ Assets/Scripts/OnMessageButtonClicked.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class OnMessageButtonClicked : MonoBehaviour
+{
+    public string messageValue;
+
+    UniWebView webView;
+
+    public void onMessageButtonClicked()
+    {
+        var webViewGameObject = new GameObject("UniWebView");
+        webView = webViewGameObject.AddComponent<UniWebView>();
+
+        webView.insets = new UniWebViewEdgeInsets(0, 0, 0, 0);
+        webView.toolBarShow = true;
+        webView.Load("http://" + messageValue);
+        webView.Show();
+    }
+}
